f6ab6c746ea9a9f6dfa5dceded5e51cfd7161779
Samael1624 <sulaxanambade.dg@gmail.com>
2025-01-23 11:17:21 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,7 @@ function App() {
               }
             />
             <Route
-              path="/"
+              path="/*"
               element={
                 <ProtectedRoutes>
                   <ProtectedLayout>
