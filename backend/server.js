150a54175e5e51de575309eb88dcef40b46aba9a
Samael1624 <sulaxanambade.dg@gmail.com>
2025-01-30 13:49:08 +0530

    build path changes

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 // API routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
