adae719d0a25a22b209cfd6029722a98811c0618
Samael1624 <sulaxanambade.dg@gmail.com>
2025-06-23 13:46:53 +0530

    newlinesIssue

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
+// Parse service account from environment variable
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// Replace escaped newlines with real ones
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-module.exports = admin;
