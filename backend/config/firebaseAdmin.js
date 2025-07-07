9e8af9949cf4b6a51cf48a6a9f1619d1325162ce
Samael1624 <sulaxanambade.dg@gmail.com>
2025-07-07 12:01:57 +0530

    .

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -4,8 +4,9 @@ const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 // Replace escaped newlines with real ones
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+module.exports = admin;
